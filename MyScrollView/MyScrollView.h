1cc30e2cbb09204470da45537ebe35a73224b1ce
Colin Russell <colinrussell101@gmail.com>
2018-04-23 11:51:42 -0400

    added the colour views to the main view and started to work on MyScrollView

---
+++ MyScrollView/MyScrollView.h
@@ -0,0 +1,13 @@
+//
+//  MyScrollView.h
+//  MyScrollView
+//
+//  Created by Colin on 2018-04-23.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyScrollView : UIView
+
+@end
--- MyScrollView/ViewController.m
+++ MyScrollView/ViewController.m
@@ -7,22 +7,43 @@
 //
 
 #import "ViewController.h"
+#import "MyScrollView.h"
 
 @interface ViewController ()
-
+@property (nonatomic) UIView *myView;
+@property (nonatomic, weak) MyScrollView *scrollView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.myView = [[UIView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:self.myView];
+    [self setupColorViews];
+    
 }
 
+- (void)setupColorViews {
+    UIView *redView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
+    redView.backgroundColor = [UIColor redColor];
+    [self.myView addSubview:redView];
+    UIView *greenView = [[UIView alloc] initWithFrame:CGRectMake(150, 150, 150, 200)];
+    greenView.backgroundColor = [UIColor greenColor];
+    [self.myView addSubview:greenView];
+    UIView *blueView = [[UIView alloc] initWithFrame:CGRectMake(40, 400, 200, 150)];
+    blueView.backgroundColor = [UIColor blueColor];
+    [self.myView addSubview:blueView];
+    UIView *yellowView = [[UIView alloc] initWithFrame:CGRectMake(100, 600, 180, 160)];
+    yellowView.backgroundColor = [UIColor yellowColor];
+    [self.myView addSubview:yellowView];
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    CGRect frame = self.view.bounds;
+    frame.origin.y += 100;
+    self.myView.bounds = frame;
 }
 
 
